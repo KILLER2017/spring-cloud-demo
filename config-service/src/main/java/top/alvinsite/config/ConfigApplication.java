2a0a1989747d381d32a3d7deb602e91e1a775259
ALVIN <543046534@qq.com>
2021-01-19 19:16:27 +0800

    添加配置文件

--- config-service/src/main/java/top/alvinsite/config/ConfigApplication.java
+++ config-service/src/main/java/top/alvinsite/config/ConfigApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.support.AbstractScmAccessor;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -14,7 +15,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigApplication {
-
+    AbstractScmAccessor abstractScmAccessor;
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
--- user-service/src/main/java/top/alvinsite/user/UserApplication.java
+++ user-service/src/main/java/top/alvinsite/user/UserApplication.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Repository;
 )
 @SpringCloudApplication
 public class UserApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
