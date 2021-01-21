21ccb174e550bec7eccf9d64901e466abba0a098
ALVIN <543046534@qq.com>
2021-01-21 16:38:37 +0800

    添加配置文件

--- user-service/src/main/java/top/alvinsite/user/controller/TestController.java
+++ user-service/src/main/java/top/alvinsite/user/controller/TestController.java
@@ -2,6 +2,7 @@ package top.alvinsite.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @email 54304634@qq.com
  * @date 2021/1/18 15:15
  */
+@RefreshScope
 @Slf4j
 @RequestMapping
 @RestController
 public class TestController {
+
 
     @Value("${from}")
     private String from;
